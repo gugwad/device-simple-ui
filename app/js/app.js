7e0ed19c04bd3b80c49ac6cd27e93f1dccd5b7d5
nribeka <nyoman@openmrs.org>
2014-04-04 10:24:23 -0400

    Working on device navigation

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-var muzimaDevice = angular.module('muzimaDevice', ['ngRoute', 'muzimaDevice.filters', 'muzimaDevice.services', 'muzimaDevice.directives', 'muzimaDevice.controllers']);
+var muzimaDevice = angular.module('muzimaDevice', ['ngRoute', 'muzimaDevice.filters', 'muzimaDevice.services', 'muzimaDevice.directives', 'muzimaDevice.controllers', 'ui.bootstrap']);
 
 muzimaDevice.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/person', {templateUrl: 'partials/person.html', controller: 'PersonCtrl'});
     $routeProvider.when('/persons', {templateUrl: 'partials/persons.html', controller: 'PersonsCtrl'});
     $routeProvider.when('/device', {templateUrl: 'partials/device.html', controller: 'DeviceCtrl'});
-    $routeProvider.when('/setting', {templateUrl: 'partials/setting.html', controller: 'SettingCtrl'});
+    $routeProvider.when('/devices', {templateUrl: 'partials/devices.html', controller: 'DevicesCtrl'});
+    $routeProvider.when('/device', {templateUrl: 'partials/deviceType.html', controller: 'DeviceTypeCtrl'});
+    $routeProvider.when('/deviceTypes', {templateUrl: 'partials/deviceTypes.html', controller: 'DeviceTypesCtrl'});
+    $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'SettingsCtrl'});
     $routeProvider.when('/help', {templateUrl: 'partials/help.html', controller: 'HelpCtrl'});
     $routeProvider.otherwise('/person');
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,21 +4,52 @@
 
 angular.module('muzimaDevice.controllers', [])
     .controller('PersonsCtrl', ['apiLocation', '$http', '$rootScope', '$scope', function (apiLocation, $http, $rootScope, $scope) {
-        $rootScope.person = true;
-        $http.get(apiLocation + "/api/person")
+        $rootScope.navigation = "person";
+        $scope.count = 0;
+        $scope.pageSize = 10;
+        $rootScope.currentPage = 1;
+        $http.get(apiLocation + "/api/person?query=" + $scope.search + "&max=" + $scope.pageSize + "&offset=" + ($scope.pageSize * ($rootScope.currentPage - 1)))
             .success(function (data) {
                 $scope.persons = data.results;
+                $scope.count = data.count;
             });
+
+        $scope.$watch('currentPage', function (newValue, oldValue) {
+            if (newValue != oldValue) {
+                $http.get(apiLocation + "/api/person?query=" + $scope.search + "&max=" + $scope.pageSize + "&offset=" + ($scope.pageSize * (newValue - 1)))
+                    .success(function (data) {
+                        $scope.persons = data.results;
+                        $scope.count = data.count;
+                    });
+            }
+        }, true);
+
+        $scope.$watch('search', function (newValue, oldValue) {
+            if (newValue != oldValue) {
+                $rootScope.currentPage = 1;
+                $http.get(apiLocation + "/api/person?query=" + $scope.search + "&max=" + $scope.pageSize + "&offset=" + ($scope.pageSize * ($rootScope.currentPage - 1)))
+                    .success(function (data) {
+                        $scope.persons = data.results;
+                        $scope.count = data.count;
+                    });
+            }
+        }, true);
     }])
     .controller('PersonCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.person = true;
+        $rootScope.navigation = "person";
+    }])
+    .controller('DevicesCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $rootScope.navigation = "device";
     }])
     .controller('DeviceCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.device = true;
+        $rootScope.navigation = "device";
     }])
-    .controller('SettingCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.setting = true;
+    .controller('DeviceTypesCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $rootScope.navigation = "deviceTypes";
+    }])
+    .controller('SettingsCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $rootScope.navigation = "settings";
     }])
     .controller('HelpCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.help = true;
+        $rootScope.navigation = "help";
     }]);
