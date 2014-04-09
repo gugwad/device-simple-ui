0fb24b6a3bab2f20cb226e101e703bc056157bd6
nribeka <nyoman@openmrs.org>
2014-04-09 16:40:31 -0400

    Adding edit person mechanism

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,9 @@
 var muzimaDevice = angular.module('muzimaDevice', ['ngRoute', 'muzimaDevice.filters', 'muzimaDevice.services', 'muzimaDevice.directives', 'muzimaDevice.controllers', 'ui.bootstrap']);
 
 muzimaDevice.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
     $routeProvider.when('/person/:personId', {templateUrl: 'partials/person.html', controller: 'PersonCtrl'});
+    $routeProvider.when('/person/:personId/edit', {templateUrl: 'partials/editPerson.html', controller: 'EditPersonCtrl'});
     $routeProvider.when('/persons', {templateUrl: 'partials/persons.html', controller: 'PersonsCtrl'});
     $routeProvider.when('/device/:deviceId', {templateUrl: 'partials/device.html', controller: 'DeviceCtrl'});
     $routeProvider.when('/devices', {templateUrl: 'partials/devices.html', controller: 'DevicesCtrl'});
@@ -17,25 +19,33 @@ muzimaDevice.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 muzimaDevice.value("$dataProvider", "http://localhost:8080/device-simple");
-muzimaDevice.service('$person', function($http, $dataProvider) {
-    var searchPerson = function(search, pageSize, currentPage) {
+muzimaDevice.service('$person', function ($http, $dataProvider) {
+    var searchPerson = function (search, pageSize, currentPage) {
         return $http.get($dataProvider + "/api/person?query=" + search + "&max=" + pageSize + "&offset=" + (pageSize * (currentPage - 1)));
     };
-    var getPerson = function(personId) {
+    var getPerson = function (personId) {
         return $http.get($dataProvider + "/api/person/" + personId);
     };
+    var updatePerson = function(person) {
+        return $http({
+            method: "PUT",
+            data: person,
+            url: $dataProvider + "/api/person/" + person.id
+        });
+    }
     return {
         searchPerson: searchPerson,
-        getPerson: getPerson
+        getPerson: getPerson,
+        updatePerson: updatePerson
     }
 });
 
-muzimaDevice.service('$device', function($http, $dataProvider) {
-    var searchDevice = function(search, pageSize, currentPage) {
+muzimaDevice.service('$device', function ($http, $dataProvider) {
+    var searchDevice = function (search, pageSize, currentPage) {
         return $http.get($dataProvider + "/api/device?query=" + search + "&max=" + pageSize + "&offset=" + (pageSize * (currentPage - 1)));
     };
-    var getDevice = function(personId) {
-        return $http.get($dataProvider + "/api/device/" + personId);
+    var getDevice = function (deviceId) {
+        return $http.get($dataProvider + "/api/device/" + deviceId);
     };
     return {
         searchDevice: searchDevice,
@@ -43,7 +53,41 @@ muzimaDevice.service('$device', function($http, $dataProvider) {
     }
 });
 
+muzimaDevice.service('$deviceType', function ($http, $dataProvider) {
+    var searchDeviceType = function (search, pageSize, currentPage) {
+        return $http.get($dataProvider + "/api/deviceType?query=" + search + "&max=" + pageSize + "&offset=" + (pageSize * (currentPage - 1)));
+    };
+    var getDeviceType = function (deviceTypeId) {
+        return $http.get($dataProvider + "/api/deviceType/" + deviceTypeId);
+    };
+    return {
+        searchDeviceType: searchDeviceType,
+        getDeviceType: getDeviceType
+    }
+});
+
 muzimaDevice.run(function ($http) {
     $http.defaults.headers.common['Accept'] = "application/json";
     $http.defaults.headers.common['Content-Type'] = "application/json";
+});
+
+muzimaDevice.run(function ($rootScope) {
+    $rootScope.numericalOrder = function (number) {
+        switch (number) {
+            case 1:
+                return "First";
+                break;
+            case 2:
+                return "Second";
+                break;
+            case 3:
+                return "Third";
+                break;
+            case 4:
+                return "Fourth";
+                break;
+            default:
+                return "Other";
+        }
+    };
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,9 @@
 /* Controllers */
 
 angular.module('muzimaDevice.controllers', [])
+    .controller('HomeCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $rootScope.navigation = "home";
+    }])
     .controller('PersonsCtrl', ['$person', '$rootScope', '$scope',
         function ($person, $rootScope, $scope) {
             $rootScope.navigation = "person";
@@ -38,6 +41,7 @@ angular.module('muzimaDevice.controllers', [])
         }])
     .controller('PersonCtrl', ['$person', '$filter', '$rootScope', '$scope', '$routeParams',
         function ($person, $filter, $rootScope, $scope, $routeParams) {
+            $scope.edit = false;
             $scope.personId = $routeParams.personId;
             $person.getPerson($scope.personId)
                 .success(function (data) {
@@ -65,6 +69,73 @@ angular.module('muzimaDevice.controllers', [])
                         }
                     }
                 });
+        }])
+    .controller('EditPersonCtrl', ['$person', '$filter', '$rootScope', '$scope', '$routeParams', '$location',
+        function ($person, $filter, $rootScope, $scope, $routeParams, $location) {
+            $scope.person = {};
+            $scope.personId = $routeParams.personId;
+            $person.getPerson($scope.personId)
+                .success(function (data) {
+                    $scope.person = data;
+                    if (data.hasOwnProperty("gender")) {
+                        if (data["gender"] === 'M') {
+                            $scope.male = true;
+                        } else {
+                            $scope.female = true;
+                        }
+                    }
+
+                    $scope.preferredName = {};
+                    if ($scope.person.hasOwnProperty('personNames')) {
+                        $scope.preferredName = $scope.person["personNames"][0];
+                        angular.forEach($scope.person["personNames"], function (personName) {
+                            personName.active = false;
+                            if (personName.hasOwnProperty('preferred')) {
+                                var preferred = personName["preferred"];
+                                if (preferred === 'true') {
+                                    $scope.preferredName = personName;
+                                }
+                            }
+                        });
+                        // make the first tab as active
+                        $scope.person["personNames"][0].active = true;
+                    }
+                    if ($scope.person.hasOwnProperty("personAddresses")) {
+                        angular.forEach($scope.person["personAddresses"], function(personAddress) {
+                            personAddress.active = false;
+                        });
+                        // make the first tab as active
+                        $scope.person["personAddresses"][0].active = true;
+                    }
+                });
+            $scope.addPersonName = function() {
+                if ($scope.person.hasOwnProperty("personNames")) {
+                    angular.forEach($scope.person["personNames"], function(personName) {
+                        personName.active = false;
+                    });
+                    $scope.person["personNames"].push({
+                        active: true
+                    });
+                }
+            };
+            $scope.addPersonAddress = function() {
+                if ($scope.person.hasOwnProperty("personAddresses")) {
+                    angular.forEach($scope.person["personAddresses"], function(personAddress) {
+                        personAddress.active = false;
+                    });
+                    $scope.person["personAddresses"].push({
+                        active: true
+                    });
+                }
+            };
+            $scope.savePerson = function() {
+                $person.updatePerson($scope.person)
+                    .success(function(data) {
+                        if (data.hasOwnProperty("id")) {
+                            $location.path("/person/" + data["id"]);
+                        }
+                    });
+            }
         }])
     .controller('DevicesCtrl', ['$device', '$rootScope', '$scope',
         function ($device, $rootScope, $scope) {
@@ -115,11 +186,59 @@ angular.module('muzimaDevice.controllers', [])
                     }
                 });
         }])
-    .controller('DeviceTypesCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.navigation = "deviceTypes";
-    }])
+    .controller('DeviceTypesCtrl', ['$deviceType', '$rootScope', '$scope',
+        function ($deviceType, $rootScope, $scope) {
+            $rootScope.navigation = "deviceType";
+            $scope.count = 0;
+            $scope.pageSize = 10;
+            $rootScope.currentPage = 1;
+            $deviceType.searchDeviceType($scope.search, $scope.pageSize, $rootScope.currentPage)
+                .success(function (data) {
+                    $scope.deviceTypes = data.results;
+                    $scope.count = data.count;
+                });
+
+            $scope.$watch('currentPage', function (newValue, oldValue) {
+                if (newValue != oldValue) {
+                    $deviceType.searchDeviceType($scope.search, $scope.pageSize, newValue)
+                        .success(function (data) {
+                            $scope.deviceTypes = data.results;
+                            $scope.count = data.count;
+                        });
+                }
+            }, true);
+
+            $scope.$watch('search', function (newValue, oldValue) {
+                if (newValue != oldValue) {
+                    $rootScope.currentPage = 1;
+                    $deviceType.searchDeviceType(newValue, $scope.pageSize, $rootScope.currentPage)
+                        .success(function (data) {
+                            $scope.deviceTypes = data.results;
+                            $scope.count = data.count;
+                        });
+                }
+            }, true);
+        }])
+    .controller('DeviceTypeCtrl', ['$deviceType', '$filter', '$rootScope', '$scope', '$routeParams',
+        function ($deviceType, $filter, $rootScope, $scope, $routeParams) {
+            $scope.deviceTypeId = $routeParams.deviceTypeId;
+            $deviceType.getDeviceType($scope.deviceTypeId)
+                .success(function (data) {
+                    $scope.deviceType = data;
+                    $scope.groupedDetails = {};
+                    var deviceDetails = data["deviceDetails"];
+                    for (var i = 0; i < deviceDetails.length; i++) {
+                        var deviceDetail = deviceDetails[i];
+                        var category = deviceDetail["category"];
+                        $scope.groupedDetails[category] = $filter('filter')(deviceDetails, {"category": category});
+                    }
+                });
+            $scope.editDeviceType = function () {
+                console.log("Editing device type");
+            }
+        }])
     .controller('SettingsCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-        $rootScope.navigation = "settings";
+        $rootScope.navigation = "setting";
     }])
     .controller('HelpCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
         $rootScope.navigation = "help";
